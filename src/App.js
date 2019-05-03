9b403dd86afd802c64f4b3587c5609ea83bf6ea8
Tom Quinn <tomquinn@hotmail.co.nz>
2019-05-03 16:18:27 +1200

    modify state at runtime with setState()

--- src/App.js
+++ src/App.js
@@ -9,7 +9,29 @@ class App extends Component {
       { name: 'Tom', age: 27 },
       { name: 'Max', age: 30 },
       { name: 'Stephanie', age: 40 }
-    ]
+    ],
+    fullToggled: false
+  }
+
+  switchNameHandler = () => {
+    if (this.state.fullToggled === true) {
+      this.setState( {   
+        persons: [
+          { name: 'Tom', age: 27 },
+          { name: 'Max', age: 30 },
+          { name: 'Steph', age: 40 }
+        ],
+        fullToggled: false  })
+    }
+    else {
+      this.setState( {   
+        persons: [
+          { name: 'Thomas', age: 27 },
+          { name: 'Maximillian', age: 30 },
+          { name: 'Stephanie', age: 40 }
+        ],
+        fullToggled: true  })
+    }
   }
 
   render() {
@@ -17,7 +39,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is a paragraph element.</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
