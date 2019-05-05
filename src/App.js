dc452f0feb87ac40344cd5185259dfe1f7595b75
Tom Quinn <tomquinn@hotmail.co.nz>
2019-05-05 22:34:09 +1200

    implement two-way binding on an html element

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
         fullToggled: false  })
     }
     else {
-      this.setState( {   
+      this.setState({   
         persons: [
           { name: 'Thomas', age: 27 },
           { name: 'Maximillian', age: 30 },
@@ -34,15 +34,36 @@ class App extends Component {
     }
   }
 
+  nameChangedHandler = (event) => {
+    this.setState( {   
+      persons: [
+        { name: 'Tom', age: 27 },
+        { name: event.target.value, age: 30 },
+        { name: 'Steph', age: 40 }
+      ],
+      fullToggled: false  })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is a paragraph element.</p>
         <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler}>
+        </Person>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          changed={this.nameChangedHandler}>
+        </Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}>
+        </Person>
       </div>
     );
   }
