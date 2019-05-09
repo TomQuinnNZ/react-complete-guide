29d88cac6f499ae3c0cfa5d88c1e2b9d84f335ed
Tom Quinn <tomquinn@hotmail.co.nz>
2019-05-09 16:44:51 +1200

    ammend state object based on person ID instead of index

--- src/App.js
+++ src/App.js
@@ -6,21 +6,36 @@ class App extends Component {
 
   state = {
     persons: [
-      { name: 'Tom', age: 27 },
-      { name: 'Max', age: 30 },
-      { name: 'Stephanie', age: 40 }
+      { id: 1, name: 'Tom', age: 27 },
+      { id: 2, name: 'Max', age: 30 },
+      { id: 3, name: 'Stephanie', age: 40 }
     ],
     fullToggled: false,
     showPeople: true
   }
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+
+    // match the argument id with a person object
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      // grab a COPY of the person object in question
+      ...this.state.persons[personIndex]
+    }
+
+    // set the person "name" property to our new input value
+    person.name = event.target.value;
+
+    // make a COPY of the persons state object, and add our modified person to it
+    const people = [...this.state.persons];
+    people[personIndex] = person;
+
+    // set the state object to the value of our new persons array.
     this.setState( {   
-      persons: [
-        { name: 'Tom', age: 27 },
-        { name: event.target.value, age: 30 },
-        { name: 'Steph', age: 40 }
-      ],
+      persons: people,
       fullToggled: false  })
   }
 
@@ -61,7 +76,9 @@ class App extends Component {
             return <Person
             click={() => this.deletePersonHandler(index)}
             name={person.name} 
-            age={person.age} />
+            age={person.age}
+            key={person.id}
+            changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       )
