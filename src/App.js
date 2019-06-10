6ea05b1d2ea4907cc08610de9fa9b388adde25e3
Tom Quinn <tomquinn@hotmail.co.nz>
2019-06-10 16:18:40 +1200

    add an ErrorBoundary() class to wrap components which may fail

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Radium, { StyleRoot } from 'radium';
 import classes from './App.module.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
 
@@ -75,12 +76,12 @@ class App extends Component {
       peopleList = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
+            return <ErrorBoundary><Person
             click={() => this.deletePersonHandler(index)}
             name={person.name} 
             age={person.age}
             key={person.id}
-            changed={(event) => this.nameChangedHandler(event, person.id)} />
+            changed={(event) => this.nameChangedHandler(event, person.id)} /></ErrorBoundary>
           })}
         </div>
       );
