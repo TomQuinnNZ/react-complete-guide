b86589f7ca511aa1633e375362346e924ee8cecc
Tom Quinn <tomquinn@hotmail.co.nz>
2019-06-17 16:31:03 +1200

    whole bunch of styling

--- src/components/People/Person/Person.js
+++ src/components/People/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { MdDelete } from 'react-icons/md';
 
 import styles from './Person.module.css';
 
@@ -6,8 +7,14 @@ class Person extends Component {
     render() {
         return (
             <div className={styles.Person}>
-                <p className= {styles.PersonDesc} 
-                    onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old.
+                <p className= {styles.PersonDesc}>I'm {this.props.name} and I am {this.props.age} years old.
+                </p>
+                <p className={styles.deleteButton}>
+                    <MdDelete 
+                        className={styles.deleteIcon} 
+                        size='2em'
+                        onClick={this.props.click}>Delete
+                    </MdDelete>
                 </p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed}/>
